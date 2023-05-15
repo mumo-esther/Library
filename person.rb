77f2eef9bb093d91b44ab8e4f63f0737409f9231
mumo-esther <mumoesther2021@gmail.com>
2023-05-15 18:54:28 +0300

    Fix errors

--- person.rb
+++ person.rb
@@ -1,23 +1,22 @@
 class Person
-    attr_reader :id, :name, :age
-  
-    def initialize(id, age, parent_permission: true, name: 'Unknown')
-      @id = id
-      @name = name
-      @age = age
-      @parent_permission = parent_permission
-    end
-  
-    attr_writer :name, :age
-  
-    def can_use_services?
-      of_age? || @parent_permission
-    end
-  
-    private
-  
-    def of_age?
-      @age >= 18
-    end
+  attr_reader :id, :name, :age
+
+  def initialize(id, age, parent_permission: true, name: 'Unknown')
+    @id = id
+    @name = name
+    @age = age
+    @parent_permission = parent_permission
   end
-  
+
+  attr_writer :name, :age
+
+  def can_use_services?
+    of_age? || @parent_permission
+  end
+
+  private
+
+  def of_age?
+    @age >= 18
+  end
+end
