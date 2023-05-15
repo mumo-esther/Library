f117bcee09bc6de2f86a137f9967e86ef295d4d1
mumo-esther <mumoesther2021@gmail.com>
2023-05-15 18:51:27 +0300

    Fix cop errors

--- person.rb
+++ person.rb
@@ -1,28 +1,23 @@
 class Person
-  attr_reader :id, :name, :age
-
-  def initialize(id, age, parent_permission: true, name: "Unknown")
-    @id = id
-    @name = name
-    @age = age
-    @parent_permission = parent_permission
+    attr_reader :id, :name, :age
+  
+    def initialize(id, age, parent_permission: true, name: 'Unknown')
+      @id = id
+      @name = name
+      @age = age
+      @parent_permission = parent_permission
+    end
+  
+    attr_writer :name, :age
+  
+    def can_use_services?
+      of_age? || @parent_permission
+    end
+  
+    private
+  
+    def of_age?
+      @age >= 18
+    end
   end
-
-  def name=(name)
-    @name = name
-  end
-
-  def age=(age)
-    @age = age
-  end
-
-  def can_use_services?
-    of_age? || @parent_permission
-  end
-
-  private
-
-  def of_age?
-    @age >= 18
-  end
-end
+  
