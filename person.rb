146422df20f1796dd2c731f3aa5c6f3352110a82
mumo-esther <mumoesther2021@gmail.com>
2023-05-17 16:56:00 +0300

    Add decorator classes

--- person.rb
+++ person.rb
@@ -1,4 +1,10 @@
-class Person
+class Nameable
+  def correct_name
+    raise NotImplementedError, "Subclasses must implement 'correct_name' method."
+  end
+end
+
+class Person < Nameable
   attr_reader :id, :age
   attr_accessor :name
 
@@ -9,6 +15,10 @@ class Person
     @parent_permission = parent_permission
   end
 
+  def correct_name
+    name
+  end
+
   def can_use_services?
     of_age? || @parent_permission
   end
@@ -17,5 +27,29 @@ class Person
 
   def of_age?
     @age >= 18
+  end
+end
+
+class Decorator < Nameable
+  def initialize(nameable)
+    @nameable = nameable
+  end
+
+  def correct_name
+    @nameable.correct_name
+  end
+end
+
+class CapitalizeDecorator < Decorator
+  def correct_name
+    super.capitalize
+  end
+end
+
+class TrimmerDecorator < Decorator
+  MAX_LENGTH = 10
+
+  def correct_name
+    super[0, MAX_LENGTH]
   end
 end
