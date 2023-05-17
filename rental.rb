7cadc2fe82c3d038145d2e435f6df359a4ec9e26
mumo-esther <mumoesther2021@gmail.com>
2023-05-17 17:41:49 +0300

    Fix rubocop errors

--- rental.rb
+++ rental.rb
@@ -1,11 +1,10 @@
 class Rental
-    attr_accessor :date
-    attr_reader :person, :book
-  
-    def initialize(date, person, book)
-      @date = date
-      @person = person
-      @book = book
-    end
+  attr_accessor :date
+  attr_reader :person, :book
+
+  def initialize(date, person, book)
+    @date = date
+    @person = person
+    @book = book
   end
-  
+end
